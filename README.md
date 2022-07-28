994844a1f30421de8d0b48e51a4fb5e635db4a7a
P-man2976 <ikmy1119924@gmail.com>
2022-07-28 21:55:48 +0900

    Add error handling, save user profile

--- README.md
+++ README.md
@@ -10,6 +10,17 @@
 1. `cp .env.placeholder .env`
 1. `TWITTER_OAUTH2_CLIENT_ID`、`TWITTER_OAUTH2_CLIENT_SECRET`、`CALLBACK_URL`、`PORT`に任意の値をセット
 1. `yarn node --loader ts-node/esm index.ts` or `tsc && node ./dist/index.js`
+1. `Authorize by visiting this URL:` で表示されるURLでOAuth認証
+1. `Type twitter username:` にTwitterのユーザー名（@から始まるもの）を入力
+
+## 出力
+
+`tweets/[YYYY]-[MM]-[DD]_[HH]-[mm]-[ss]_[Twitter_username].json` に保存
+
+|プロパティ|値|
+|---|---|
+|`user`|ユーザーのプロフィール。|
+|`tweets`|ユーザーのツイートのリスト。|
 
 ## 例
 
--- index.ts
+++ index.ts
@@ -1,10 +1,15 @@
 import "dotenv/config";
-import { TwitterApi } from "twitter-api-v2";
+import { TTweetv2TweetField, TTweetv2UserField, TwitterApi } from "twitter-api-v2";
 import fs from "fs/promises";
 import * as readline from "node:readline";
 import express from "express";
 import chalk from "chalk";
 import ora from "ora";
+
+interface Options {
+	tweet_fields: TTweetv2TweetField[];
+	user_fields: TTweetv2UserField[];
+}
 
 const app = express();
 const rl = readline.createInterface({
@@ -41,94 +46,115 @@ app.get("/", async (req, res) => {
 	});
 	res.status(200).send("Successfully authenticated. Type username in the terminal.");
 
-	rl.question("Type twitter username: ", async (username) => {
-		const user = await client.v2.userByUsername(username);
+	const options: Options = {
+		tweet_fields: [
+			"attachments",
+			"author_id",
+			"context_annotations",
+			"conversation_id",
+			"created_at",
+			"entities",
+			"geo",
+			"id",
+			"in_reply_to_user_id",
+			"lang",
+			// "non_public_metrics",
+			// "organic_metrics",
+			"possibly_sensitive",
+			// "promoted_metrics",
+			"public_metrics",
+			"referenced_tweets",
+			"reply_settings",
+			"source",
+			"text",
+			"withheld",
+		],
+		user_fields: [
+			"created_at",
+			"description",
+			"entities",
+			"id",
+			"location",
+			"name",
+			"pinned_tweet_id",
+			"profile_image_url",
+			"protected",
+			"public_metrics",
+			"url",
+			"username",
+			"verified",
+			"withheld",
+		],
+	};
 
-		if (!user.data) {
-			console.log(`The user ${chalk.bold('(@' + username + ')')} was ${chalk.redBright("not found")}.`);
+	rl.question("Type twitter username: ", async (username) => {
+		const spinner = ora(`Fetching user data of ${chalk.bold(`(@${username})`)}...`).start();
+		
+		try {
+			const user = await client.v2.userByUsername(username, {
+				expansions: ["pinned_tweet_id"],
+				"tweet.fields": options.tweet_fields,
+				"user.fields": options.user_fields,
+			});
+	
+			if (!user.data) {
+				throw `The user ${chalk.bold(`(@${username})`)} was not found.`
+			}
+
+			spinner.text = `Collecting ${chalk.bold(user.data.name + " (@" + user.data.username + ")")} tweets...`;
+
+			const userTimeline = await client.v2.userTimeline(user.data.id, {
+				expansions: [
+					"attachments.media_keys",
+					"attachments.poll_ids",
+					"author_id",
+					"entities.mentions.username",
+					"geo.place_id",
+					"in_reply_to_user_id",
+					"referenced_tweets.id",
+					"referenced_tweets.id.author_id",
+				],
+				max_results: 100,
+				"media.fields": [
+					"alt_text",
+					"duration_ms",
+					"height",
+					"media_key",
+					"non_public_metrics",
+					"organic_metrics",
+					"preview_image_url",
+					"public_metrics",
+					"type",
+					"url",
+					"width",
+				],
+				"place.fields": ["contained_within", "country", "country_code", "full_name", "geo", "id", "name", "place_type"],
+				"poll.fields": ["duration_minutes", "end_datetime", "id", "options", "voting_status"],
+				"tweet.fields": options.tweet_fields,
+				"user.fields": options.user_fields,
+			});
+
+			const tweets = [];
+
+			for await (const tweet of userTimeline) {
+				tweets.push(tweet);
+			}
+			const fileName = 
+				`${new Date().toLocaleDateString().replaceAll("/", "-")}_${new Date().toLocaleTimeString().replaceAll(":", "-")}_${user.data.username}.json`;
+
+			const result = {
+				user: user.data,
+				tweets,
+			};
+			await fs.writeFile(`./tweets/${fileName}`, JSON.stringify(result, null, "\t"));
+
+			spinner.succeed();
+			console.log(`${chalk.greenBright(tweets.length)} tweet${tweets.length > 1 ? "s" : ""} saved to ${chalk.cyanBright(fileName)}.`);
+			process.exit();
+		} catch (error) {
+			spinner.fail();
+			console.log(`Collecting tweets of ${chalk.bold(`(@${username})`)} ${chalk.redBright.bold("failed")}:\n${chalk.redBright(error)}`);
 			process.exit();
 		}
-		const spinner = ora((`Collecting ${chalk.bold(user.data.name + " (@" + user.data.username + ")")} tweets...`)).start()
-
-		const userTimeline = await client.v2.userTimeline(user.data.id, {
-			expansions: [
-				"attachments.media_keys",
-				"attachments.poll_ids",
-				"author_id",
-				"entities.mentions.username",
-				"geo.place_id",
-				"in_reply_to_user_id",
-				"referenced_tweets.id",
-				"referenced_tweets.id.author_id",
-			],
-			max_results: 100,
-			"media.fields": [
-				"alt_text",
-				"duration_ms",
-				"height",
-				"media_key",
-				"non_public_metrics",
-				"organic_metrics",
-				"preview_image_url",
-				"public_metrics",
-				"type",
-				"url",
-				"width",
-			],
-			"place.fields": ["contained_within", "country", "country_code", "full_name", "geo", "id", "name", "place_type"],
-			"poll.fields": ["duration_minutes", "end_datetime", "id", "options", "voting_status"],
-			"tweet.fields": [
-				"attachments",
-				"author_id",
-				"context_annotations",
-				"conversation_id",
-				"created_at",
-				"entities",
-				"geo",
-				"id",
-				"in_reply_to_user_id",
-				"lang",
-				// 'non_public_metrics',
-				// 'organic_metrics',
-				"possibly_sensitive",
-				//  'promoted_metrics',
-				"public_metrics",
-				"referenced_tweets",
-				"reply_settings",
-				"source",
-				"text",
-				"withheld",
-			],
-			"user.fields": [
-				"created_at",
-				"description",
-				"entities",
-				"id",
-				"location",
-				"name",
-				"pinned_tweet_id",
-				"profile_image_url",
-				"protected",
-				"public_metrics",
-				"url",
-				"username",
-				"verified",
-				"withheld",
-			],
-		});
-
-		const result = [];
-
-		for await (const tweet of userTimeline) {
-			result.push(tweet);
-		}
-		const fileName = 
-			`${new Date().toLocaleDateString().replaceAll("/", "-")}-${new Date().toLocaleTimeString().replaceAll(":", "-")}_${user.data.username}.json`;
-
-		await fs.writeFile(`./tweets/${fileName}`, JSON.stringify(result, null, "\t"));
-
-		spinner.succeed();
-		console.log(`${chalk.greenBright(result.length)} tweet${result.length > 1 ? 's' : ''} saved to ${chalk.blueBright(fileName)}.`);
-		process.exit();
 	});
 });
